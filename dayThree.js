f282f9e6dcf6b85d0f950309b0b3289f32b321a9
Michael Wetterauer <mjw0486@gmail.com>
2020-12-27 16:11:38 -0500

    Day three part two solution

--- dayThree.js
+++ dayThree.js
@@ -14,5 +14,26 @@ function dayThree(array) {
   return count;
 }
 
+function dayThreePartTwo(array) {
+  const counts = [];
+  const slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]];
+  slopes.forEach((slope) => {
+    let count = 0;
+    let columnIndex = 0;
+    const columnIncrement = slope[0];
+    const rowIncrement = slope[1];
+    for (let i = 0; i < array.length; i += rowIncrement) {
+      if (array[i][columnIndex] === '#') {
+        count += 1;
+      }
+      columnIndex = (columnIndex + columnIncrement) % array[i].length;
+    }
+    counts.push(count);
+  });
+  return counts.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
+}
+
 const inputArray = input.split('\n').slice(0, -1);
+
 dayThree(inputArray);
+dayThreePartTwo(inputArray);
